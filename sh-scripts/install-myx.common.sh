82a94e20068e699f283195235e1c307dc5554c0d
Alexander Kharichev <myx@myx.co.nz>
2020-03-17 01:37:01 +0200

    * better rsync

--- sh-scripts/install-myx.common.sh
+++ sh-scripts/install-myx.common.sh
@@ -77,8 +77,8 @@ if test `id -u` = 0 ; then
 	
     if [ "`which rsync`" -a -d "/usr/local/share/myx.common/" ] ; then
     	echo "Using: rsync"
-    	rsync -rltOPi --no-motd --progress "$T_DIR/bin/myx.common" "/usr/local/bin/myx.common"
-    	rsync -rltOPi --no-motd --progress --delete "$T_DIR/share/myx.common/" "/usr/local/share/myx.common/"
+    	rsync -rltOi --no-motd "$T_DIR/bin/myx.common" "/usr/local/bin/myx.common"
+    	rsync -rltOi --no-motd --delete "$T_DIR/share/myx.common/" "/usr/local/share/myx.common/"
     else
     	echo "Using: tar-tar"
 	   	RSYNC(){ tar -cpf - -C "$1" `ls "$1"` | tar -xvpf - -C "/usr/local/" ; }
